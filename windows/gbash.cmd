b37e7d73f008c63459deb5f6a74cc11873323451
Good Good <Good Good>
2017-03-24 02:57:11 -0400

    added gbash

---
+++ windows/gbash.cmd
@@ -0,0 +1,5 @@
+rem Author: Xiao Liang <13267.okk@gmail.com>
+rem gbash stands for good bash which fixes bug that some keys can't be read by bash 
+rem Ref: https://github.com/Maximus5/ConEmu/issues/629
+
+bash -cur_console:p1
